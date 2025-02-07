b62e73d7953d0931062838c46ab7f6795d01d276
sergiolttz <sergiolattanzio05@gmail.com>
2025-02-07 14:31:43 -0400

    busqueda de profesor arreglada

--- app/views.py
+++ app/views.py
@@ -2,49 +2,24 @@ from django.shortcuts import render
 from .models import Estudiante, Profesor
 from django.http import HttpResponse
 
-def index(request):  
+def index(request):
     return render(request, 'index.html')
 
 def pagina_formulario(request):
     return render(request, 'form.html')
 
 def busqueda_profesor(request):
-    
-    if request.GET['profesor']:
-        
-        nombre_solicitado = request.GET ['profesor']
-
-        try:
-            profesor = Profesor.objects.get(nombre_y_apellido = nombre_solicitado)
-        except Exception as ex:
-            mensaje = f'Ocurrió un error: {ex}'
-        else:
-            mensaje = f'Años de experiencia {profesor.tiempo_exp} <br> Asignatura: {profesor.Asignaura}'
-        finally:
-            return HttpResponse('<h1>'+mensaje+'</h1>')
-    
+    if request.GET.get('profesor'):
+        nombre_solicitado = request.GET['profesor']
+        lista_personas = Profesor.objects.filter(nombre_y_apellido__icontains=nombre_solicitado)
+        return render(request, 'resultados.html', {'busqueda': nombre_solicitado, 'personas': lista_personas, 'tipo': 'profesor'})
     else:
-        return HttpResponse('Busqueda vacia')
-    
-lista_estudiantes = Estudiante.objects.all()
+        return HttpResponse('búsqueda vacía')
 
 def busqueda_estudiante(request):
-    if request.GET['estudiante']:
+    if request.GET.get('estudiante'):
         nombre_solicitado = request.GET['estudiante']
-
-        lista_personas = Estudiante.objects.filter(nombre_y_apellido__icontains = nombre_solicitado)
-
-        ...
-
-        lista_personas = []
-        for i in lista_estudiantes:
-            if i.nombre_y_apellido == nombre_solicitado:
-                lista_personas.append(i)
-        ...
-
-        return render(request, 'resultados.html', {'busqueda': nombre_solicitado, 'personas': lista_personas})
+        lista_personas = Estudiante.objects.filter(nombre_y_apellido__icontains=nombre_solicitado)
+        return render(request, 'resultados.html', {'busqueda': nombre_solicitado, 'personas': lista_personas, 'tipo': 'estudiante'})
     else:
-        return HttpResponse('busqueda vacia')
-
-
-# Create your views here.
+        return HttpResponse('búsqueda vacía')
